df3481f4d2b2f71edb1077f37d324746e62d2c9d
mirageowl <ydoga123@gmail.com>
2019-04-29 18:20:27 +0300

    Very small express code added

---
+++ practice-app/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.use(express.static('static'))
+app.use(express.urlencoded({extended: true})) // req.body nin çalışması için gerekli
+
+app.get("/", (req, res) => { res.send("Home") })
+app.get('/register', function(req, res) {
+  res.render("register")
+})
+app.post('/register', function(req, res) {
+  console.log(req.body) // formdaki inputları bu şekilde alıyoruz
+  // create database entry
+})
+app.listen(port, () => console.log('Listening here'))
