a74eba23469e9fe6fa436a318340cea499fa7243
hasan-yaman <hasannyaman@gmail.com>
2019-05-01 16:00:04 +0300

    Create an API that takes same parameters as Alpha Vantage API and returns the same data.

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 3000;
+
+
+// Parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var url = "https://www.alphavantage.co/query";
+
+
+// Use middleware to set the default Content-Type to json
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
+
+
+app.get('/',(req,res) => {
+
+	// Extract the parameters.
+	var parameters = {
+		function: req.query.function,
+		symbol: req.query.symbol,
+		interval: req.query.interval,
+		apikey: 'FZD9JFSVI7D6HY5E'
+	};
+
+	// Get dara from the Aplha Vontage API and return the results.
+	request({url:url, qs:parameters}, function(error,response, body) {
+		if(!error && response.statusCode == 200) {
+			res.status(200).send(body)
+		}
+	}); 
+});
+
+app.listen(port, () => console.log('Example app listening on port ${port}!'));
+
