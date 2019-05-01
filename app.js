ece7992fa78489e8f5fd474d171a9f0d0d8ce564
HasanYaman <hasannyaman@gmail.com>
2019-05-01 16:29:41 +0300

    Small fixes according to feedbacks

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var url = "https://www.alphavantage.co/query";
+var stock_api_url = "https://www.alphavantage.co/query";
 
 
 // Use middleware to set the default Content-Type to json
@@ -22,6 +22,7 @@ app.use(function (req, res, next) {
 app.get('/',(req,res) => {
 
 	// Extract the parameters.
+	// For detail of parameters: https://www.alphavantage.co/documentation/
 	var parameters = {
 		function: req.query.function,
 		symbol: req.query.symbol,
@@ -30,7 +31,7 @@ app.get('/',(req,res) => {
 	};
 
 	// Get dara from the Aplha Vontage API and return the results.
-	request({url:url, qs:parameters}, function(error,response, body) {
+	request({url:stock_api_url, qs:parameters}, function(error,response, body) {
 		if(!error && response.statusCode == 200) {
 			res.status(200).send(body)
 		}
