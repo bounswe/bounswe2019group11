fda226000374f0b7f7883ba9a10ab4cf3473ed5f
Metin Dumandag <metin@hazelcast.com>
2019-05-04 20:10:21 +0300

    add a simple home page layout and navigation bar

--- practice-app/index.js
+++ practice-app/index.js
@@ -1,24 +1,23 @@
-// This file is like that just for testing purposes. I will update it after the reviews
-
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-dotenv.config();
-
 app.set('view engine', 'pug');
 app.set('views', './views');
+
 app.use(express.static('static'));
 app.use(express.urlencoded({extended: true}));
 
-
-app.use('/api/exchangerate', require('./routes/exchange_rate_api'));
-app.use('/exchangerate', require('./routes/exchange_rate'));
 app.use('/stock', require('./routes/stock'));
+app.use('/exchangerate', require('./routes/exchange_rate'));
+app.use('/api/exchangerate', require('./routes/exchange_rate_api'));
 
-const url = 'mongodb+srv://dbUser:Rku5ANPmO0t2srkw@mongotest-dnhun.mongodb.net/test?retryWrites=true';
-mongoose.connect(url, {useNewUrlParser: true});
-app.listen(port, () => console.log('...'));
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => console.log(`Started on port ${port}`));
