e47f3961dfd0f9d188f1f1cfeacf1cd12d1398b8
unknown <alperen.degirmenci@boun.edu.tr>
2019-05-05 23:41:43 +0300

    added percentage change feature to the exchange rates API

--- practice-app/routes/exchange_rate_api.js
+++ practice-app/routes/exchange_rate_api.js
@@ -61,6 +61,23 @@ router.get('/avg', (req, res) => {
     calculateAverage(res, start_date, end_date, from, to);
 });
 
+router.get('/percentage', (req, res) => {
+    let from = req.query.from || 'TRY';
+    let to = req.query.to;
+    let change_date = req.query.change_date || moveDate('', 0);
+    let prev_day =  moveDate(change_date, -1);
+
+    if (to == null || to == '') {
+        res.status(400).send({
+            'error': '\'to\' parameter cannot be empty!'
+        });
+        return;
+    }
+    
+    calculatePercentage(res, change_date, prev_day, from, to);
+    
+});
+
 
 function sendRequestToEndpoint(res, from, to) {
     const reqOptions = {'url': process.env.EXCHANGE_RATE_API_URL, 'qs': {'base': from, 'symbols': to}};
@@ -111,6 +128,35 @@ function calculateAverage(res, start_date, end_date, from, to) {
             }
             avg = avg / count;
             res.send({'from': from, 'to': to, 'start_date':start_date, 'end_date': end_date, 'average': avg});
+        }
+    });
+}
+
+function calculatePercentage(res, change_date, prev_day, from, to) {
+    const reqOptions = {'url': process.env.EXCHANGE_RATE_API_HISTORY_URL, 'qs': {'start_at': prev_day, 'end_at': change_date, 'base': from, 'symbols': to}};
+    request(reqOptions, (err, response, body) => {
+        if (err) {
+            // If something went wrong during the request, send it to the user
+            res.status(400).send({'error': '' + err});
+        } else if (response.statusCode !== 200) {
+            // If the endpoint returns an error code (like invalid from/to symbols)
+            // send it directly to the user
+            res.status(400).send(body);
+        } else {
+            const result = JSON.parse(body);
+            var perc = 0;
+            var firstValue = 0;
+            firstValue = result['rates'][moveDate(change_date,0)][to];
+            var secondValue = 0;
+            secondValue = result['rates'][moveDate(prev_day,0)][to];
+            perc = (firstValue-secondValue)/secondValue *100;
+            /*for (var i = 0; i < dateDiff(start_date, end_date); i++) {
+                if(result['rates'][moveDate(start_date, i)]){
+                    avg = avg + result['rates'][moveDate(start_date, i)][to];
+                    count ++;
+                }
+            }*/
+            res.send({'from': from, 'to': to, 'change_date':change_date, '% change': perc});
         }
     });
 }
