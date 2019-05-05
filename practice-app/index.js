51ce4ab2749045ca46efb5e8540edccd13c6d8bf
alkimtoprak <44143037+alkimtoprak@users.noreply.github.com>
2019-05-06 00:26:41 +0300

    Update index.js

--- practice-app/index.js
+++ practice-app/index.js
@@ -1,74 +1,38 @@
-const express = require('express')
-const date = require('date-and-time');
-const app = express()
-const port = 3000
-//const router = express.Router();
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://dbAdmin:dbAdminPass@traderzcluster-ro4z4.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
-mongoose.set('useCreateIndex', true);
-const Stocks = require('./stockSchema');
-const request = require('request');
-const stock_api_url = "https://www.alphavantage.co/query";
-const StocksData = mongoose.model('StocksData', Stocks);
+require('dotenv').config({path: __dirname + '/.env'});
 
-app.set('view engine', 'pug')
-app.set('views', './views')
-app.use(express.static('static'))
-app.use(express.urlencoded({extended: true})) // req.body nin çalışması için gerekli
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-app.get('/stock', function(req, res) {
-  res.render("stock")
-})
-
-app.post('/stock',(req,res) => {
-  console.log(req.body);
-
-  // Extract the parameters.
-  // For detail of parameters: https://www.alphavantage.co/documentation/
-  var parameters = {
-    function : req.body.function,
-    symbol : req.body.symbol,
-    interval : req.body.interval,
-    apikey: 'FZD9JFSVI7D6HY5E'
-  };
-
-  var final_url = stock_api_url + "?function=" + req.body.function + "&symbol=" + req.body.symbol + "&interval=" + req.body.interval + "&apikey=" + req.body.apikey;
-
-  // Get data from the Alpha Vontage API and return the results.
-  request({url:stock_api_url, qs:parameters}, function(error,response, body) {
-    if(!error && response.statusCode == 200) {
-      res.status(200).send(body)
-    }
-    var obj = JSON.parse(body);
-    var firstInterval = date.parse(obj['Meta Data']['3. Last Refreshed'], 'YYYY-MM-DD HH:mm:ss');
-    var dataInterval = req.body.interval.replace(/(^\d+)(.+$)/i,'$1');
-    
-    for( var i = firstInterval ; obj["Time Series (" +req.body.interval +")" ][date.format(i, 'YYYY-MM-DD HH:mm:ss')] ; i = date.addMinutes(i, -dataInterval ) ) {
-      StocksData.create(
-        {
-          information : obj['Meta Data']['1. Information'],
-          symbol : obj['Meta Data']['2. Symbol'],
-          lastRefreshed :  obj['Meta Data']['3. Last Refreshed'],
-          interval: obj['Meta Data']['4. Interval'],
-          outputSize: obj['Meta Data']['5. Output Size'],
-          timeZone: obj['Meta Data']['6. Time Zone'],
-          dateData: date.format(i, 'YYYY-MM-DD HH:mm:ss'),
-          open: obj["Time Series (" +req.body.interval +")" ][date.format(i, 'YYYY-MM-DD HH:mm:ss')]['1. open'],
-          high: obj["Time Series (" +req.body.interval +")" ][date.format(i, 'YYYY-MM-DD HH:mm:ss')]['2. high'],
-          low: obj["Time Series (" +req.body.interval +")" ][date.format(i, 'YYYY-MM-DD HH:mm:ss')]['3. low'],
-          close: obj["Time Series (" +req.body.interval +")" ][date.format(i, 'YYYY-MM-DD HH:mm:ss')]['4. close'],
-          volume: obj["Time Series (" +req.body.interval +")" ][date.format(i, 'YYYY-MM-DD HH:mm:ss')]['5. volume']
-        }
-      );
-    }
-    
-  }); 
-  
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const db = require('./helpers/db');
+db
+    .connect()
+    .on('error', console.error)
+    .on('disconnected', db.connect)
+    .once('open', () => {
+        console.log("Database connected");
 });
 
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: 'application/json'
+}));
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
+app.use(express.static('static'));
+app.use(express.urlencoded({extended: true}));
 
+app.use('/stock', require('./routes/stock'));
+app.use('/exchangerate', require('./routes/exchange_rate'));
+app.use('/api/exchangerate', require('./routes/exchange_rate_api'));
+app.use('/register', require('./routes/register'))
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => console.log(`Started on port ${port}`));
+
+module.exports = app; // for testing
