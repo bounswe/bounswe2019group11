0ffed87530cfcd595809436bd7f24cc5271c00be
Metin Dumandag <metin@hazelcast.com>
2019-10-02 21:40:17 +0300

    add status code to signup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/sign-up', async (req, res) => {
     try {
         const { name, surname, email, password, idNumber, iban } = req.body;
-        res.send(await authService.signUp(name, surname, email, password, idNumber, iban));
+        res.status(200).send(await authService.signUp(name, surname, email, password, idNumber, iban));
     } catch (e) {
         res.status(500).send({ error: `DatabaseError: ${e}`});
     }
