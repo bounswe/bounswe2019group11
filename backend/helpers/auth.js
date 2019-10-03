d158c202cb81aa917bf64fa6d339e63cc4042a29
Metin Dumandag <metin@hazelcast.com>
2019-10-03 23:15:33 +0300

    implement simple id and iban validators

--- backend/helpers/auth.js
+++ backend/helpers/auth.js
@@ -4,12 +4,14 @@ module.exports.ROLES = {
     ADMIN: 'ADMIN'
 };
 
+const ibanRegex = new RegExp('^([A-Z]{2}[ \\-]?[0-9]{2})(?=(?:[ \\-]?[A-Z0-9]){9,30}$)' +
+    '((?:[ \\-]?[A-Z0-9]{3,5}){2,7})([ \\-]?[A-Z0-9]{1,3})?$');
+const idNumberRegex = new RegExp('^[0-9]{11}$');
+
 module.exports.validateIdNumber = (idNumber) => {
-    // TODO implement this
-    return idNumber;
+    return idNumberRegex.test(idNumber);
 };
 
 module.exports.validateIban = (iban) => {
-    // TODO implement this
-    return iban;
+    return ibanRegex.test(iban);
 };
