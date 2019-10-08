5b8eec00606b10bbdbd699053901ebf87ab46c53
aysusayin <aysusayin@gmail.com>
2019-10-08 16:16:28 +0300

    Changed the message on home page.

--- android/app/src/main/java/com/papel/ui/home/HomeViewModel.java
+++ android/app/src/main/java/com/papel/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Welcome to Papel");
     }
 
     public LiveData<String> getText() {
