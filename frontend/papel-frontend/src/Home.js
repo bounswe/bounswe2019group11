f66fc831a4e015cde2f2f1bfdb0b55b230c424b6
mirageowl <ydoga123@gmail.com>
2019-10-15 11:52:43 +0300

    Frontend router setup, Trading Equipment page layout done, login and signup handle requests with jQuery

---
+++ frontend/papel-frontend/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style/bootstrap/css/bootstrap.min.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <h1>Wow!</h1>
+    );
+  }
+}
+export default Home;
