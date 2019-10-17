964d58fc65dbb68982f705780f85281008b42bda
hasan <hasannyaman@gmail.com>
2019-10-17 20:30:31 +0300

    Add stock endpoints.

--- android/app/src/main/java/com/papel/Constants.java
+++ android/app/src/main/java/com/papel/Constants.java
@@ -2,4 +2,7 @@ package com.papel;
 
 public class Constants {
     public static final String CHART_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=FZD9JFSVI7D6HY5E";
+    public static final String LOCALHOST = "http://192.168.2.188:3000/";
+    public static final String STOCK = "stock/";
+
 }
