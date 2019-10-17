b74d07c48d9cc058bf14930285e24bfb422437ce
Alkim Ece Toprak <alkim.toprak@sap.com>
2019-10-18 01:34:59 +0300

    added services and routes

---
+++ backend/services/article.js
@@ -0,0 +1,27 @@
+const Article = require('../models/article');
+
+module.exports.getAll = async ()=>{
+    return await Article.find();
+};
+
+module.exports.getByID = async (articleID) => {
+    return await Article.findOne({
+        _id: articleID,
+    });
+};
+
+module.exports.getByUserID = async (userID) => {
+    return await Article.findOne({
+        authorID: userID,
+    });
+};
+
+module.exports.create = async (article) => {
+    const newArticle = {...article};
+    const createdArticle = await Stock.create(newArticle);
+    return createdStock;
+}; 
+
+module.exports.delete = async (articleID) => {
+    return await Article.findByIdAndDelete(articleID);
+}; 
