6b4dd5837e8a639da37927f078a7c12096a1734a
İbrahim Kamacı <kamacibrahim@gmail.com>
2019-10-17 03:42:37 +0300

    3rd party api url and api key added

--- backend/services/tradingEquipment.js
+++ backend/services/tradingEquipment.js
@@ -1,11 +1,26 @@
 const TradingEquipment = require('../models/tradingEquipment')
 const StockEquipment = require ('../models/stock');
+
+const getStockDailyPrice = async (symbol) => {
+
+}
+
 module.exports.getAll = async ()=>{
     return await TradingEquipment.find();
-}
+};
 
 module.exports.getById= async (id)=>{
     return await TradingEquipment.findOne({
+        _id:id
+    });
+}
+
+module.exports.getStockAll = async ()=>{
+    return await StockEquipment.find();
+};
+
+module.exports.getStockById= async (id)=>{
+    return await StockEquipment.findOne({
         _id:id
     });
 }
