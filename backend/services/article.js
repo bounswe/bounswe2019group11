7159a3fae8e9ff3bfed0e779437dba875bccddb2
cumhurkilic <36170065+cumhurkilic@users.noreply.github.com>
2019-10-18 04:32:29 +0300

    fixed getByUserID

--- backend/services/article.js
+++ backend/services/article.js
@@ -11,7 +11,7 @@ module.exports.getByID = async (articleID) => {
 };
 
 module.exports.getByUserID = async (userID) => {
-    return await Article.findOne({
+    return await Article.find({
         authorID: userID,
     });
 };
