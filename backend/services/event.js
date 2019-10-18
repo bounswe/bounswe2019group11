e35c40be7a6645a7493f86cbab3555a6de4429ed
cumhurkilic <36170065+cumhurkilic@users.noreply.github.com>
2019-10-18 03:57:04 +0300

    Add files via upload

--- backend/services/event.js
+++ backend/services/event.js
@@ -2,12 +2,24 @@ const Event = require("../models/Event");
 
 
 
+module.exports.getByID = async (eventID) => {
+    return await Event.findOne({
+        _id: eventID,
+    });
+};
 
+module.exports.getAll = async ()=>{
+    return await Event.find();
+};
 
-
-
+module.exports.getByCountry = async (country_) => {
+    return await Event.find({
+        country: country_,
+    });
+};
 
 module.exports.create = async (title, body, comment, date, authorId, rank, voterNumber, country) => {
+    
     const event = await Event.create({
         title, body, comment, date, authorId, rank, voterNumber, country
     });
@@ -15,5 +27,7 @@ module.exports.create = async (title, body, comment, date, authorId, rank, voter
 };
 
 module.exports.delete = async (eventID) => {
-    return await Event.findByIdAndDelete(articleID);
+    return await Event.findByIdAndDelete(eventID);
 }; 
+
+
