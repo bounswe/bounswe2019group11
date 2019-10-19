545ab947ad1e7b07e7776e5221d8ed7a3f3558b6
hasan <hasannyaman@gmail.com>
2019-10-19 22:43:38 +0300

    Connect sign up page to backend

--- android/app/src/main/java/com/papel/Constants.java
+++ android/app/src/main/java/com/papel/Constants.java
@@ -2,10 +2,16 @@ package com.papel;
 
 public class Constants {
     public static final String LOCALHOST = "http://192.168.2.188:3000/";
+
     public static final String STOCK = "stock/";
     public static final String PORTFOLIO = "portfolio/";
     public static final String PORTFOLIO_USER = "portfolio/user/";
+    public static final String SIGN_UP = "auth/sign-up";
+    public static final String LOGIN = "auth/login";
+
     public static final String TEST_USER_ID = "5da076d955d9b93a7308f3d7";
     public static final int DAILY_CHART = 0;
     public static final int MONTHLY_CHART = 1;
+
+    public static final String VALIDATION_ERROR = "ValidationError";
 }
--- android/app/src/main/java/com/papel/ui/home/ui/login/LoginActivity.java
+++ android/app/src/main/java/com/papel/ui/home/ui/login/LoginActivity.java
@@ -1,31 +1,19 @@
 package com.papel.ui.home.ui.login;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.IntentService;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -33,138 +21,118 @@ import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.papel.Constants;
 import com.papel.MainActivity;
 import com.papel.R;
-import com.papel.ui.home.ui.login.LoginViewModel;
-import com.papel.ui.home.ui.login.LoginViewModelFactory;
+import com.papel.ui.utils.DialogHelper;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
     private boolean isLogin = true;
+    private boolean isTrader = false;
+
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText nameInput;
+    private EditText surnameInput;
+    private CheckBox traderCheckBox;
+    private EditText ibanInput;
+    private EditText idInput;
+    private Button changeScreen;
+    private Button sendReq;
+    private ProgressBar progressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
-        EditText emailInput = findViewById(R.id.emailInput);
-        EditText passwordInput = findViewById(R.id.passwordInput);
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        nameInput = findViewById(R.id.nameInput);
+        surnameInput = findViewById(R.id.surnameInput);
+        ibanInput = findViewById(R.id.ibanInput);
+        idInput = findViewById(R.id.idInput);
+        progressBar = findViewById(R.id.progressBar);
 
-        final EditText nameInput = findViewById(R.id.nameInput);
-        final EditText surnameInput = findViewById(R.id.surnameInput);
-        final EditText ibanInput = findViewById(R.id.ibanInput);
-        final EditText idInput = findViewById(R.id.idInput);
-        final Button changeScreen = findViewById(R.id.changeScreen);
-        final Button sendReq = findViewById(R.id.sendReqButton);
+        changeScreen = findViewById(R.id.changeScreen);
+        sendReq = findViewById(R.id.sendReqButton);
+
+        traderCheckBox = findViewById(R.id.checkBox);
+        traderCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+               isTrader = b;
+               if (b) {
+                   showTraderSignUp();
+               } else {
+                   hideTraderSignUp();
+               }
+            }
+        });
 
         final Intent intent = new Intent(this, MainActivity.class);
-
 
         changeScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
                 if (isLogin) {
                     // Click on "change to sign up" button.
-
-                    nameInput.setVisibility(View.VISIBLE);
-                    surnameInput.setVisibility(View.VISIBLE);
-                    ibanInput.setVisibility(View.VISIBLE);
-                    idInput.setVisibility(View.VISIBLE);
-                    changeScreen.setText(getString(R.string.changeLogin));
-                    sendReq.setText(getString(R.string.signUp_button));
+                    showSignUp();
                     isLogin = false;
 
                 } else {
                     // Click on "change to sign in" button.
-
-                    nameInput.setVisibility(View.INVISIBLE);
-                    surnameInput.setVisibility(View.INVISIBLE);
-                    ibanInput.setVisibility(View.INVISIBLE);
-                    idInput.setVisibility(View.INVISIBLE);
-                    changeScreen.setText(getString(R.string.changeSignup));
-                    sendReq.setText(getString(R.string.login_button));
+                    showLogin();
                     isLogin = true;
                 }
             }
         });
 
-        // To test, please write your local ip address and don't forget to run servers locally.
-        final String url = "http://192.168.43.108:3000/auth/sign-up";
-        final RequestQueue queue = Volley.newRequestQueue(this);
 
         sendReq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sendReq.setClickable(false);
+                progressBar.setVisibility(View.VISIBLE);
+
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
 
                 if (isLogin) {
-                    startActivity(intent);
-                } else {
-                    final JSONObject jsonBody = new JSONObject();
-                    try {
-                        // TODO Get parameters from EditText's
-                        jsonBody.put("name", "name1");
-                        jsonBody.put("surname", "surname1");
-                        jsonBody.put("email", "hasan09912120d0@gmail.com");
-                        jsonBody.put("password", "strongPa$.$word");
-                        jsonBody.put("idNumber", "12345678910");
-                        jsonBody.put("iban", "TR33 0006 1005 1978 6457 8413 26");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                    if (email.length() == 0 ||password.length() == 0) {
+                        DialogHelper.showBasicDialog(LoginActivity.this,"Error","You must fill all fields.");
+                    } else {
+                        sendLoginRequest(email, password);
                     }
 
-                    StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            Log.i("Response", response);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            String responseBody = new String(error.networkResponse.data, StandardCharsets.UTF_8);
-                            // TODO convert responseBody to JSON Object and display appropriate error messages
-                            Log.d("Error", responseBody);
-                        }
-                    }) {
-                        @Override
-                        public byte[] getBody() throws AuthFailureError {
-                            return jsonBody.toString().getBytes();
-                        }
+                } else {
+                    String name = nameInput.getText().toString();
+                    String surname = surnameInput.getText().toString();
+                    String id = idInput.getText().toString();
+                    String iban = ibanInput.getText().toString();
 
-                        @Override
-                        public String getBodyContentType() {
-                            return "application/json";
+                    if (isTrader) {
+                        if (email.length() == 0 || password.length() == 0 ||name.length() == 0 || surname.length() == 0 || id.length() == 0 || iban.length() == 0) {
+                            DialogHelper.showBasicDialog(LoginActivity.this, "Error", "You must fill all fields.");
                         }
-                    };
+                    } else {
+                        if (email.length() == 0 || password.length() == 0 ||name.length() == 0 || surname.length() == 0) {
+                            DialogHelper.showBasicDialog(LoginActivity.this, "Error", "You must fill all fields.");
+                        }
+                        id = "";
+                        iban = "";
+                    }
 
-                    queue.add(request);
-                    signUpDialog();
-                    nameInput.setVisibility(View.INVISIBLE);
-                    surnameInput.setVisibility(View.INVISIBLE);
-                    ibanInput.setVisibility(View.INVISIBLE);
-                    idInput.setVisibility(View.INVISIBLE);
-                    changeScreen.setText(getString(R.string.changeSignup));
-                    sendReq.setText(getString(R.string.login_button));
-                    isLogin = true;
+                    sendSignUpRequest(email, password, name, surname, id, iban);
                 }
             }
 
@@ -173,7 +141,180 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void signUpDialog() {
+    private void showLogin() {
+        clearTexts();
+        emailInput.setVisibility(View.VISIBLE);
+        passwordInput.setVisibility(View.VISIBLE);
+        nameInput.setVisibility(View.INVISIBLE);
+        surnameInput.setVisibility(View.INVISIBLE);
+        traderCheckBox.setVisibility(View.INVISIBLE);
+        traderCheckBox.setChecked(false);
+        idInput.setVisibility(View.INVISIBLE);
+        ibanInput.setVisibility(View.INVISIBLE);
+
+        changeScreen.setText(getText(R.string.changeSignup));
+        sendReq.setText(getText(R.string.login_button));
+    }
+
+
+    private void showSignUp() {
+        clearTexts();
+        emailInput.setVisibility(View.VISIBLE);
+        passwordInput.setVisibility(View.VISIBLE);
+        nameInput.setVisibility(View.VISIBLE);
+        surnameInput.setVisibility(View.VISIBLE);
+        traderCheckBox.setVisibility(View.VISIBLE);
+        changeScreen.setText(getText(R.string.changeLogin));
+        sendReq.setText(getText(R.string.signUp_button));
+
+    }
+
+
+    private void clearTexts() {
+        emailInput.setText("");
+        passwordInput.setText("");
+        nameInput.setText("");
+        surnameInput.setText("");
+        idInput.setText("");
+        ibanInput.setText("");
+    }
+
+    private void showTraderSignUp() {
+        idInput.setVisibility(View.VISIBLE);
+        ibanInput.setVisibility(View.VISIBLE);
+        idInput.setText("");
+        ibanInput.setText("");
+    }
+
+    private void hideTraderSignUp() {
+        idInput.setVisibility(View.INVISIBLE);
+        ibanInput.setVisibility(View.INVISIBLE);
+        idInput.setText("");
+        ibanInput.setText("");
+    }
+
+    private void sendSignUpRequest(String email, String password, String name, String surname, String id, String iban) {
+        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+        String url = Constants.LOCALHOST + Constants.SIGN_UP;
+        final JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("name", name);
+            jsonBody.put("surname", surname);
+            jsonBody.put("email", email);
+            jsonBody.put("password", password);
+            // TODO change hardcoded strings
+            jsonBody.put("idNumber", "12345678910");
+            jsonBody.put("iban", "TR33 0006 1005 1978 6457 8413 26");
+            JSONObject location = new JSONObject();
+            location.put("latitude", "41.085987");
+            location.put("longitude", "29.044008");
+            jsonBody.put("location", location);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.INVISIBLE);
+                sendReq.setClickable(true);
+                // Success
+                validationDialog();
+                showLogin();
+                isLogin = true;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.INVISIBLE);
+                sendReq.setClickable(true);
+                NetworkResponse networkResponse = error.networkResponse;
+                if(networkResponse != null) {
+                    String data = new String(networkResponse.data);
+                    try{
+                        JSONObject errorObject = new JSONObject(data);
+                        String name = errorObject.getString("name");
+                        String message = errorObject.getString("message");
+                        if (name.equals(Constants.VALIDATION_ERROR)) {
+                            JSONArray cause = errorObject.getJSONArray("cause");
+                            message = "";
+                            for (int i=0;i<cause.length();i++) {
+                                JSONObject c = cause.getJSONObject(i);
+                                message = message + c.getString("message");
+                            }
+                        }
+                        DialogHelper.showBasicDialog(LoginActivity.this, "Error",message);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return jsonBody.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+
+        requestQueue.add(request);
+
+    }
+
+
+    private void sendLoginRequest(String email, String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+        String url = Constants.LOCALHOST + Constants.LOGIN;
+        final JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("email", email);
+            jsonBody.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.INVISIBLE);
+                sendReq.setClickable(true);
+                NetworkResponse networkResponse = error.networkResponse;
+                if(networkResponse != null) {
+                    String data = new String(networkResponse.data);
+                    try{
+                        JSONObject errorObject = new JSONObject(data);
+                        String message = errorObject.getString("message");
+                        DialogHelper.showBasicDialog(LoginActivity.this, "Error",message);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return jsonBody.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+
+        requestQueue.add(request);
+
+    }
+
+
+    private void validationDialog() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setMessage("Please check your e-mail for verification.");
         dialog.setTitle("Sign up successful.");
