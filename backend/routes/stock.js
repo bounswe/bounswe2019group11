9ee3699a32a4ade6826b67087c2956365e29168c
cumhurkilic <36170065+cumhurkilic@users.noreply.github.com>
2019-11-24 23:20:05 +0300

    Add files via upload

--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -35,5 +35,100 @@ router.post('/', async (req, res) => {
         res.status(503).json(e)
     }
 });
+router.post('/:id/comment', isAuthenticated, async (req, res) => {
+    try {
+        const stockId = req.params.id;
+        const userId = req.token && req.token.data && req.token.data._id;
+        const body = req.body.body;
+        await stockService.postComment(stockId, userId, body);
+        res.sendStatus(200);
+    } catch (err) {
+        if (err.name === 'StockNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'UserNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'ValidationError') {
+            const causes = [];
+            for (const field in err.errors) {
+                if (err.errors[field].message === 'InvalidBody') {
+                    causes.push(errors.INVALID_BODY());
+                } else {
+                    causes.push(errors.UNKNOWN_VALIDATION_ERROR(err.errors[field]));
+                }
+            }
+            res.status(400).send(errors.VALIDATION_ERROR(causes));
+        } else {
+            res.status(500).send(errors.INTERNAL_ERROR(err));
+        }
+    }
+});
+
+router.get('/:id/comment/:commentId', async (req, res) => {
+    try {
+        const stockId = req.params.id;
+        const commentId = req.params.commentId;
+        const comment = await stockService.getComment(stockId, commentId);
+        res.status(200).send(comment);
+    } catch (err) {
+        if (err.name === 'CommentNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'StockNotFound') {
+            res.status(400).send(err);
+        } else {
+            res.status(500).send(errors.INTERNAL_ERROR(err));
+        }
+    }
+});
+
+router.post('/:id/comment/:commentId', isAuthenticated, async (req, res) => {
+    try {
+        const stockId = req.params.id;
+        const authorId = req.token && req.token.data && req.token.data._id;
+        const commentId = req.params.commentId;
+        const newBody = req.body.body;
+        await stockService.editComment(stockId, authorId, commentId, newBody);
+        res.sendStatus(200);
+    } catch (err) {
+        if (err.name === 'StockNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'CommentNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'UserNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'ValidationError') {
+            const causes = [];
+            for (const field in err.errors) {
+                if (err.errors[field].message === 'InvalidBody') {
+                    causes.push(errors.INVALID_BODY());
+                } else {
+                    causes.push(errors.UNKNOWN_VALIDATION_ERROR(err.errors[field]));
+                }
+            }
+            res.status(400).send(errors.VALIDATION_ERROR(causes));
+        } else {
+            res.status(500).send(errors.INTERNAL_ERROR(err));
+        }
+    }
+});
+
+router.delete('/:id/comment/:commentId', isAuthenticated, async (req, res) => {
+    try {
+        const stockId = req.params.id;
+        const commentId = req.params.commentId;
+        const userId = req.token && req.token.data && req.token.data._id;
+        await stockService.deleteComment(stockId, commentId, userId);
+        res.sendStatus(200);
+    } catch (err) {
+        if (err.name === 'StockNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'CommentNotFound') {
+            res.status(400).send(err);
+        } else if (err.name === 'UserNotFound') {
+            res.status(400).send(err);
+        } else {
+            res.status(500).send(errors.INTERNAL_ERROR(err));
+        }
+    }
+});
 
 module.exports = router;
