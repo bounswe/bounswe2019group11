c624341b65b071a8b9893b53402a70d5b16e915b
benescakici <benescakici@gmail.com>
2019-12-21 17:32:51 +0300

    Static charts on home page are converted into dynamic.

--- frontend/papel-frontend/src/Home/StockChart2.js
+++ frontend/papel-frontend/src/Home/StockChart2.js
@@ -12,20 +12,33 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const url = app_config.api_url + "/";
 
-function StockChart2({stock}) {
-  const [currency, setCurrency] = useState(0);
-  if(!currency){$.get(url+"currency/EUR/last-month", (data) => {
-    setCurrency( data)  });
+function StockChart2({stock, stockName}) {
+  //console.log(stock)
+  //var stock_id = "5df7b5adf29d0356042b862"
+  const [stockInfo, setStockInfo] = useState(0);
+  const [stockInfoReceived, setStockInfoReceived] = useState(false);
+  if(!stockInfoReceived){
+    getRequest({
+      url: app_config.api_url + "/stock/" + stock[0],
+      success: (data) => {
+        setStockInfo(data);
+        setStockInfoReceived(true);
+      }
+      })
   }
-  console.log(currency);
-  var currencyLastValues = {};
-  currencyLastValues = currency.lastMonth;
+  //console.log("here1")
+  //console.log(stockInfo);
+  //console.log("here2")
+  var stockLastValues = {};
+  stockLastValues = stockInfo.monthlyPrice;
+  //console.log(stockLastValues)
+  //console.log("here3")
   var key;
   var days = [], months=[], years=[];
   var values = [];
   var closeDayValues = [];
-  for (key in currencyLastValues) {
-    if (currencyLastValues.hasOwnProperty(key)) {
+  for (key in stockLastValues) {
+    if (stockLastValues.hasOwnProperty(key)) {
       var date = key;
       var day = date.split("-")[2],
       month =  date.split("-")[1],
@@ -33,34 +46,35 @@ function StockChart2({stock}) {
       days.push(day);
       months.push(month);
       years.push(year);
-      values.push(currencyLastValues[key]);
+      values.push(stockLastValues[key]);
     }
   }
-  console.log(days)
-  console.log(months)
-  console.log(years)
-  console.log(currencyLastValues)
+  //console.log(days)
+  //console.log(months)
+  //console.log(years)
+  //console.log(stockLastValues)
   var i;
   for (i = 0; i < values.length; i++) {
     for(key in values[i] ){
-      if(key.includes("4")){closeDayValues.push(parseFloat(values[i][key]) ); console.log(closeDayValues);}
+      if(key.includes("4")){closeDayValues.push(parseFloat(values[i][key]) );}
     } ;
   }
   //<CurencyCh currency={code} year={years} month={months} day={days} value={closeDayValues}></CurencyCh >
   var data = [];
   for (let index = 0; index < years.length; index++) {
     data[index] = {x: new Date(years[index],months[index]-1,days[index]),	y: closeDayValues[index]}
-    console.log(data[index]);
+    //console.log(data[index]);
   }
 
   const options = {
     theme: "light1", // "light1", "dark1", "dark2"
     animationEnabled: true,
-    zoomEnabled: true,
+    zoomEnabled: false,
     title: {
-      text:"USD/EUR"
+      text:stock[2]
     },
     axisY: {
+      title: "value (in USD)",
       includeZero: false
     },
     data: [{
@@ -69,43 +83,7 @@ function StockChart2({stock}) {
       dataPoints: data
     }]
   }
-  /*
-    const options = {
-      animationEnabled: true,
-      title:{
-        text: "Walt Disney Company (The) Common Stock"
-      },
-      axisY : {
-        title: stock+"/USD",
-        includeZero: false
-      },
-      axisX : {
-        title: "Date",
-        includeZero: false
-      },
-      toolTip: {
-        shared: true
-      },
-      data: [{
-        type: "area",
-        color : "#3DA544",
-        name: "Stock2",
-        showInLegend: false,
-        dataPoints: [
-          {x: new Date(2019,10,22),	y: 148.29},
-          {x: new Date(2019,10,21),	y: 146.9},
-          {x: new Date(2019,10,20),	y: 146.93},
-          {x: new Date(2019,10,19),	y: 148.38},
-          {x: new Date(2019,10,18),	y: 147.65},
-          {x: new Date(2019,10,15),	y: 144.67},
-          {x: new Date(2019,10,14),	y: 147.15},
-          {x: new Date(2019,10,13),	y: 148.72},
-          {x: new Date(2019,10,12),	y: 138.58},
-          {x: new Date(2019,10,11),	y: 136.74}
-        ]
-      }]
-    };
-    */
+
     return (
       <Card>
         <div className="row">
