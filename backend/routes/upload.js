ee646d6ac64cdb0b57ebc65282cc83466a2bb801
ikamaci <ibrahim.kamaci@boun.edu.tr>
2019-12-22 06:02:09 +0300

    add profile picture fetch endpoint

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -2,29 +2,30 @@ const express = require('express');
 const router = express.Router();
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const multer = require('multer');
+const path = require('path');
+const dir = path.join(__dirname, '../uploads');
 const profilePictureStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './uploads/profilePictures');
     },
     filename: (req,file, cb) => {
-
         const userId = req.token && req.token.data && req.token.data._id;
-        console.log(userId);
         const name = userId+".png";
-        console.log(name);
-
-        console.log(file.path)
         cb(null, name); //req.userData._id);
     }
 });
 
 const uploadPP = multer({storage:profilePictureStorage});
 
+router.get('/avatar/:userId',async (req,res)=>{
+    const userId = req.params.userId;
+    const filePath = path.join(dir,'profilePictures/'+userId+".png");
+    res.status(200).sendFile(filePath);
+});
 
 router.post('/avatar',isAuthenticated, uploadPP.single('avatar'),async (req,res)=>{
-    const path = req.file.path;
-    const mimetype = req.file.mimetype;
-    console.log(path);
-    console.log(mimetype);
+    const userId = req.token && req.token.data && req.token.data._id;
+    const imgUri = process.env.BACKEND_URL + "/upload/avatar/" +  userId;
+    res.status(200).json({imgUri:imgUri,msg:"File uploaded successfully"});
 });
 module.exports =  router;
