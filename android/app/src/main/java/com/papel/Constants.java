bc574dbfb5a2f7a3dba059a3eb67b9bcde1bfbaf
aysusayin <aysusayin@gmail.com>
2019-12-24 00:23:18 +0300

    The functionality to show notifications is implemented.

--- android/app/src/main/java/com/papel/Constants.java
+++ android/app/src/main/java/com/papel/Constants.java
@@ -33,6 +33,7 @@ public class Constants {
     public static final String ANNOTATION = "annotation/";
     public static final String SEARCH = "search/";
     public static final String ALERT = "alert/";
+    public static final String NOTIFICATION = "notification/";
 
     public static final int DAILY_CHART = 0;
     public static final int WEEKLY_CHART = 1;
