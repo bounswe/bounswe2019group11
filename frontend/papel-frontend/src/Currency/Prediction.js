42d37519a00b40939a2dd3db2f646b582c1ee0a3
benescakici <benescakici@gmail.com>
2019-12-23 23:23:07 +0300

    Very minor fix of currency tables. Implemented prediction.

--- frontend/papel-frontend/src/Currency/Prediction.js
+++ frontend/papel-frontend/src/Currency/Prediction.js
@@ -6,6 +6,12 @@ import { app_config } from "../config";
 import { Card, Row, Column, Col, Button, Badge, Spinner } from 'react-bootstrap'
 import { faArrowUp, faLongArrowAltUp, faArrowDown } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {instanceOf} from 'prop-types'
+import {withCookies, Cookies} from 'react-cookie';
+import {postRequest} from '../helpers/request';
+import { useCookies, CookiesProvider } from 'react-cookie';
+
 var template = {
     "_id": "",
     "code": "",
@@ -38,19 +44,65 @@ var template = {
     "userPrediction": ""
 }
 function Prediction({ code }) {
-    
-var _url = app_config.api_url + "/";
+    const [cookies, setCookie, removeCookie] = useCookies(['user', 'userToken', 'pendingRequests'])
+    var _url = app_config.api_url + "/";
     const [currency, setCurrency] = useState(0);
+    const [upCount, setUpCount] = useState(0);
+    const [downCount, setDownCount] = useState(0);
     if (!currency) {
         $.get(
             _url + "currency/" + code,
             (data) => { setCurrency(data) }
         );
     }
+    console.log("currency")
+    console.log(currency)
+    console.log("template")
+    console.log(template)
     template = currency ? currency : template
-    var upCount = template.predictions[1]?template.predictions[1].count:0;
-    var downCount = template.predictions[0]?template.predictions[0].count:0;
-    
+
+    //var upCount
+    //var downCount
+
+    if(template.predictions){
+      if(template.predictions[0]){
+        if(template.predictions[0].prediction == "1"){
+          setUpCount(template.predictions[0].count);
+          if(template.predictions[1]){
+            setDownCount(emplate.predictions[1].count);
+          }
+        }
+        else{
+          setDownCount(template.predictions[0].count);
+          if(template.predictions[1]){
+            setUpCount(template.predictions[1].count);
+          }
+        }
+      }
+    }
+    //var upCount = template.predictions[1]?template.predictions[1].count:0;
+    //var downCount = template.predictions[0]?template.predictions[0].count:0;
+
+    function predictUp(event){
+      console.log("VOTE up")
+      postRequest({
+        url: app_config.api_url + "/currency/"+ code + "/predict-increase",
+        success: function()  {
+        },
+        authToken: cookies.userToken
+      });
+      //window.location.reload();
+    }
+    function predictDown(event){
+      console.log("VOTE down")
+      postRequest({
+        url: app_config.api_url + "/currency/"+ code + "/predict-decrease",
+        success: (data) => {console.log("VOTE DOWN")
+        },
+        authToken: cookies.userToken
+      });
+      //window.location.reload();
+    }
     return (
 
         <Col>
@@ -68,7 +120,7 @@ var _url = app_config.api_url + "/";
             <Row>
 
                 <Col md={{ span: 6 }} >
-                    <Button variant="primary">
+                    <Button variant="primary" onClick={predictUp}>
                         <FontAwesomeIcon name="UP" style={{ fontSize: 30, outlineBorder: 10 }} icon={faArrowUp} />&nbsp;
                     <Badge variant="light">{upCount}</Badge>
                         <span className="sr-only">unread messages</span>
@@ -77,7 +129,7 @@ var _url = app_config.api_url + "/";
                 </Col>
 
                 <Col md={{ span: 6 }} >
-                    <Button variant="primary" >
+                    <Button variant="primary" onClick={predictDown}>
                         <FontAwesomeIcon name="UP" style={{ fontSize: 30, outlineBorder: 10 }} icon={faArrowDown} />&nbsp;
                     <Badge variant="light">{downCount}</Badge>
                         <span className="sr-only">unread messages</span>
