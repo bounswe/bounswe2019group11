a4391023b233c46681c96a48880363f4ea90f1c9
hasan-yaman <hasannyaman@gmail.com>
2019-12-24 12:38:54 +0300

    Add subpages to recommendations page

---
+++ android/app/src/main/java/com/papel/ui/recommendations/NearbyUserFragment.java
@@ -0,0 +1,39 @@
+package com.papel.ui.recommendations;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.papel.R;
+
+
+public class NearbyUserFragment extends Fragment {
+
+
+    public NearbyUserFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nearby_user, container, false);
+    }
+
+
+}
--- android/app/src/main/java/com/papel/ui/recommendations/RecommendationFragment.java
+++ android/app/src/main/java/com/papel/ui/recommendations/RecommendationFragment.java
@@ -5,43 +5,27 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
 import com.papel.R;
+import com.papel.ui.profile.ProfileSubpageAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RecommendationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RecommendationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RecommendationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecommendationSubpageAdapter adapter;
+    private ViewPager pager;
+    private TabLayout tabLayout;
 
-
-    public RecommendationFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -49,6 +33,12 @@ public class RecommendationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recommendation, container, false);
+        pager = view.findViewById(R.id.pager);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        adapter = new RecommendationSubpageAdapter(getFragmentManager());
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         return view;
     }
 
