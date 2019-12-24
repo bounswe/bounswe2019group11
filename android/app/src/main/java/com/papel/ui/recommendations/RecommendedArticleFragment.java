163a93f75544c097a01d8dccac713b52b9d7933f
hasan-yaman <hasannyaman@gmail.com>
2019-12-24 14:52:56 +0300

    Update because text

--- android/app/src/main/java/com/papel/ui/recommendations/RecommendedArticleFragment.java
+++ android/app/src/main/java/com/papel/ui/recommendations/RecommendedArticleFragment.java
@@ -2,16 +2,23 @@ package com.papel.ui.recommendations;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -40,6 +47,7 @@ public class RecommendedArticleFragment extends Fragment {
     private ListView listView;
     private ArrayList<Article> articles = new ArrayList<>();
     private RecommendedArticleAdapter adapter;
+    private TextView becauseText;
 
     public RecommendedArticleFragment() {
         // Required empty public constructor
@@ -58,6 +66,7 @@ public class RecommendedArticleFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recommended_article, container, false);
         listView = view.findViewById(R.id.listView);
+        becauseText = view.findViewById(R.id.becauseText);
         fetchRecommendedArticles(getContext());
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -80,9 +89,9 @@ public class RecommendedArticleFragment extends Fragment {
                 try {
                     JSONObject responseJSON = new JSONObject(response);
                     JSONObject becauseJSON = responseJSON.getJSONObject("because");
+                    JSONArray tagsArray = becauseJSON.getJSONArray("tags");
                     JSONArray articlesJSON = responseJSON.getJSONArray("articles");
-                    // TODO use them
-                    String becauseId = becauseJSON.getString("_id");
+                    final String becauseArticleId = becauseJSON.getString("_id");
                     String becauseTitle = becauseJSON.getString("title");
                     articles.clear();
                     for(int  i = 0; i<articlesJSON.length(); i++) {
@@ -97,6 +106,37 @@ public class RecommendedArticleFragment extends Fragment {
                             articles.add(new Article(articleId,articleTitle,articleBody));
                         }
                     }
+
+
+                    String tags = "";
+                    for (int i = 0; i<tagsArray.length(); i++) {
+                        String t = (String) tagsArray.get(i);
+                        if (i == tagsArray.length() - 1) {
+                            tags = tags + t;
+                        } else {
+                            tags = tags + t + ", ";
+                        }
+                    }
+
+                    String becauseString = "Since you liked " + becauseTitle + " which has following tags: " + tags;
+                    SpannableString spannableString = new SpannableString(becauseString);
+                    spannableString.setSpan(new ClickableSpan() {
+                        @Override
+                        public void onClick(@NonNull View view) {
+                            Log.d("Info","Clicked!");
+                            Intent intent = new Intent(context,ReadArticleActivity.class);
+                            intent.putExtra("articleId",becauseArticleId);
+                            startActivity(intent);
+                        }
+                        @Override
+                        public void updateDrawState(@NonNull TextPaint ds) {
+                            super.updateDrawState(ds);
+                            ds.bgColor = Color.YELLOW;
+                            ds.setUnderlineText(false);
+                        }
+                    },16,16+becauseTitle.length(),0);
+                    becauseText.setText(spannableString);
+
                     adapter = new RecommendedArticleAdapter(context,articles);
                     listView.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
