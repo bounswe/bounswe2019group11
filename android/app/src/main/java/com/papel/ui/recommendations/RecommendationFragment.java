e0c45b188cafa33be3942b671cac8fc2f225bfdb
hasan-yaman <hasannyaman@gmail.com>
2019-12-24 12:28:09 +0300

    Add recommendations page to menu

---
+++ android/app/src/main/java/com/papel/ui/recommendations/RecommendationFragment.java
@@ -0,0 +1,55 @@
+package com.papel.ui.recommendations;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.papel.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RecommendationFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RecommendationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecommendationFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public RecommendationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_recommendation, container, false);
+        return view;
+    }
+
+}
